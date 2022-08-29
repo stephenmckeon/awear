982ae819da6cc7bcb691a8ec132ce9f7dd2de076
Stephen McKeon <stephen.mckeon@powerhrg.com>
2022-08-28 20:57:17 -0400

    Add rubocop and correct violations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class PagesController < ApplicationController
   def home
     redirect_to forecasts_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  root "pages#home"
+  root 'pages#home'
 
   resources :forecasts, only: [:index]
 end
